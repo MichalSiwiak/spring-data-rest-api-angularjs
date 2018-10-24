090459e4e2eff277ee09ffe212d25d2481cea878
Michał Siwiak <msiwiak@intercars.eu>
2018-10-24 14:05:49 +0200

    first commit

---
+++ src/test/java/com/example/addressbookdatabase/AddressBookDatabaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.addressbookdatabase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AddressBookDatabaseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
